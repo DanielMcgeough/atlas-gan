322b1ccb10328decd31dce32bd07ff26bc52ae2e
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2025-03-06 22:44:01 -0600

    Removed extra layer after no appreciable change.

--- DCGAN.py
+++ DCGAN.py
@@ -27,23 +27,25 @@ train_dataset = train_dataset.batch(batch_size)
 def build_generator():
     model = models.Sequential()
 
+    #Upsampling process
     model.add(layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(latent_dim,)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
+
+    # Reshape into 7x7x256 feature maps
     model.add(layers.Reshape((7, 7, 256)))
 
-    model.add(layers.Conv2DTranspose(256, (5, 5), strides=(1, 1), padding='same', use_bias=False)) #added layer
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-
+    # First upsampling block
     model.add(layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), padding='same', use_bias=False))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    # Second upsampling block
+    model.add(layers.Conv2DTranspose(64, (5,5), strides=(2, 2), padding='same', use_bias=False))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
+    # Output layer
     model.add(layers.Conv2DTranspose(1, (5, 5), strides=(1, 1), padding='same', use_bias=False, activation='tanh'))
 
     return model
